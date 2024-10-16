3130469de06503c0e315b8b85b891187923e973f
5050amam <matheus.castropeixoto@gmail.com>
2024-10-15 21:39:06 -0300

    Commit oficial final, adicionando exibição de json via terminal python

--- src/listar_agricultores_propriedades_pantacoes.py
+++ src/listar_agricultores_propriedades_pantacoes.py
@@ -2,6 +2,30 @@ import json
 import oracledb
 from conexao_bd_fertplan import conectar_bd
 
+def exibir_lista(caminho_arquivo):
+    """
+    Função para exibir o conteúdo de um arquivo JSON de forma legível.
+    """
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
+            dados = json.load(arquivo)
+
+        print("\n---- Conteúdo do JSON ----\n")
+        for chave, valor in dados.items():
+            print(f"Seção: {chave}")
+            print("-" * 40)
+            for item in valor:
+                for sub_chave, sub_valor in item.items():
+                    print(f"{sub_chave.capitalize()}: {sub_valor}")
+                print("\n" + "-" * 40 + "\n")
+                
+    except FileNotFoundError:
+        print(f"Erro: Arquivo '{caminho_arquivo}' não encontrado.")
+    except json.JSONDecodeError:
+        print(f"Erro: Não foi possível decodificar o conteúdo do arquivo '{caminho_arquivo}'. Verifique se é um JSON válido.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+
 def listar_agricultores_propriedades_plantacoes():
     conn, cursor, _, _, _ = conectar_bd()
     try:
@@ -48,6 +72,14 @@ def listar_agricultores_propriedades_plantacoes():
             json.dump(dados, json_file, ensure_ascii=False, indent=4)
 
         print("Dados exportados com sucesso para 'dados_exportados.json'.")
+        resposta = 0
+        while resposta != 'x' or resposta != "":
+            resposta = input("Digite 'x' para voltar ao menu ou aperte Enter para exibir dados exportados\n")
+            if resposta == 'x':
+                break
+            elif resposta == "":
+                exibir_lista('dados_exportados.json')
+                break
 
     except oracledb.DatabaseError as e:
         print("Erro ao consultar o banco de dados:", e)
@@ -56,4 +88,6 @@ def listar_agricultores_propriedades_plantacoes():
     finally:
         if conn:
             conn.close()
+    
+    
 
